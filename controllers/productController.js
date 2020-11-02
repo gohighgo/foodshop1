0960b4b92c0da280bcfbf0b74a347242574f8a06
Zenardus <seniv.artur@gmail.com>
2020-11-02 13:58:48 +0200

    Changed product create logic

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,68 +5,80 @@ const Product = require('../models/Product');
 
 
 exports.getProductsByCategory = function (request, response) {
-    Product.find({categoryId: request.params.id}, function(err, docs){
+    Product.find({ categoryId: request.params.id }, function (err, docs) {
         if (err) return response.sendStatus(400);
         response.send(docs);
-    });  
+    });
 }
 
-exports.getProductById = function (request, response){
-    Product.findOne({_id: request.params.id}, function(err, doc){
+exports.getProductById = function (request, response) {
+    Product.findOne({ _id: request.params.id }, function (err, doc) {
         if (err) return response.sendStatus(400);
         response.send(doc);
-    }); 
+    });
 }
 
 
 
 exports.createProduct = function (request, response) {
-    let filedata = request.file;
-    if (!filedata)return response.sendStatus(400);
-    const {name, price, weight, composition, categoryId} = request.body;
-    let host = 'http://' + request.headers.host + '/';
+    // let filedata = request.file;
+    // if (!filedata)return response.sendStatus(400);
+    // const {name, price, weight, composition, categoryId} = request.body;
+    // let host = 'http://' + request.headers.host + '/';
+    // Product.create({
+    //     name: name,
+    //     imagePath: host + filedata.filename,
+    //     price: price,
+    //     weight: weight,
+    //     composition: composition,
+    //     categoryId: categoryId
+    // }, function(err, doc){
+    //     if(err){
+    //         fs.unlinkSync('./uploads/' + filedata.filename);
+    //         return response.status(400).send(err);
+    //     }
+    //     response.send(doc._id);
+    // });
+    const { name, price, weight, composition, categoryId, uploadedFile } = request.body;
     Product.create({
         name: name,
-        imagePath: host + filedata.filename,
+        imagePath: uploadedFile,
         price: price,
         weight: weight,
         composition: composition,
         categoryId: categoryId
-    }, function(err, doc){
-        if(err){
-            fs.unlinkSync('./uploads/' + filedata.filename);
-            return response.status(400).send(err);
-        }
+    }, function (err, doc) {
+        if (err) return response.status(400).send(err);
         response.send(doc._id);
     });
 }
 
 
-exports.deleteById = function (request, response){
+exports.deleteById = function (request, response) {
     const id = request.params.id;
 
-    Product.findOneAndDelete({_id: id}, function(err, doc){
+    Product.findOneAndDelete({ _id: id }, function (err, doc) {
         if (err) return response.sendStatus(400);
-        fs.unlinkSync('./uploads' + doc.imagePath.replace('http://' + request.headers.host,''));
+        fs.unlinkSync('./uploads' + doc.imagePath.replace('http://' + request.headers.host, ''));
         response.send(doc._id);
     });
 }
 
 
-exports.update = function (request, response){
+exports.update = function (request, response) {
     const id = request.params.id;
     if (!request.body.name) return response.sendStatus(400);
 
-    const {name, price, weight, composition, categoryId} = request.body;
+    const { name, price, weight, composition, categoryId } = request.body;
     if (!name || !price || !weight || !composition || !categoryId) return response.sendStatus(400);
 
-    Product.findOneAndUpdate({_id: id}, {
+    Product.findOneAndUpdate({ _id: id }, {
         name: name,
         price: price,
         weight: weight,
         composition: composition,
         categoryId: categoryId
-    }, function(err){
+    }, function (err) {
         if (err) return response.status(400).send(err);
         response.sendStatus(200);
     });
